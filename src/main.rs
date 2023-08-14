6276f8f06cafa0076d4301292eec85e552ff32a4
Lily Mara <lilymara@fastmail.com>
2023-08-14 19:44:11 -0400

    basic gui

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use btleplug::{
     api::{Central, Manager as _, Peripheral as _, ScanFilter, Service, WriteType},
     platform::{Adapter, Manager, Peripheral},
 };
+use egui::{Slider, Ui};
 use eyre::{bail, Result};
 use futures::stream::StreamExt;
 use tokio::time;
@@ -22,12 +23,137 @@ const CMD_INIT_SESSION: &[u8] = &[
     0x4c, 0x54, 0x09, 0x00, 0x00, 0x53, 0x00, 0x00, 0x01, 0x00, 0x94, 0x74,
 ];
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "GVM Orchestrator",
+        native_options,
+        Box::new(|cc| Box::new(MyEguiApp::new(cc))),
+    )?;
+
+    Ok(())
+}
+
+#[derive(Default)]
+struct MyEguiApp {
+    lights: Vec<ConnectedLight>,
+}
+
+impl MyEguiApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self {
+            lights: vec![ConnectedLight::new("LED 1"), ConnectedLight::new("LED 2")],
+        }
+    }
+}
+
+impl eframe::App for MyEguiApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.vertical(|ui| {
+                for light in &mut self.lights {
+                    draw_light_group(ui, light);
+                }
+            });
+        });
+    }
+}
+
+fn draw_light_group(ui: &mut Ui, light: &mut ConnectedLight) {
+    ui.group(|ui| {
+        ui.horizontal(|ui| {
+            if light.renaming {
+                ui.text_edit_singleline(&mut light.name);
+                if ui.small_button("Ok").clicked() {
+                    light.renaming = false;
+                }
+            } else {
+                ui.toggle_value(&mut light.enabled, &light.name);
+                if ui.small_button("Rename").clicked() {
+                    light.renaming = true;
+                }
+            }
+        });
+
+        if light.enabled {
+            draw_light_settings(ui, light);
+        }
+    });
+}
+
+fn draw_light_settings(ui: &mut Ui, light: &mut ConnectedLight) {
+    ui.vertical(|ui| {
+        ui.horizontal(|ui| {
+            ui.radio_value(&mut light.mode, LightMode::Cct, "CCT");
+            ui.radio_value(&mut light.mode, LightMode::Hsi, "HSI");
+        });
+
+        ui.group(|ui| match &mut light.mode {
+            LightMode::Cct => {
+                let mut temperature_f32 = (light.temperature as f32 * 133.333 + 3200.0).floor();
+                ui.add(
+                    egui::Slider::new(&mut temperature_f32, 3200.0..=5600.0)
+                        .text("Color Temperature"),
+                );
+                light.temperature = ((temperature_f32 - 3200.0) / 133.33).round() as u8;
+
+                slider_u8(ui, &mut light.intensity, |val| {
+                    Slider::new(val, 0.0..=100.0).text("Intensity")
+                });
+            }
+            LightMode::Hsi => {
+                slider_u8(ui, &mut light.hue, |val| {
+                    Slider::new(val, 0.0..=52.0).text("Hue")
+                });
+                slider_u8(ui, &mut light.intensity, |val| {
+                    Slider::new(val, 0.0..=100.0).text("Intensity")
+                });
+            }
+        });
+    });
+}
+
+fn slider_u8(ui: &mut Ui, value: &mut u8, slider: fn(&mut f32) -> Slider) {
+    let mut value_f32 = *value as f32;
+    ui.add(slider(&mut value_f32));
+    *value = value_f32 as u8;
+}
+
+struct ConnectedLight {
+    renaming: bool,
+    name: String,
+    hue: u8,
+    intensity: u8,
+    temperature: u8,
+    mode: LightMode,
+    enabled: bool,
+}
+
+#[derive(PartialEq)]
+enum LightMode {
+    Hsi,
+    Cct,
+}
+
+impl ConnectedLight {
+    fn new(name: impl Into<String>) -> Self {
+        Self {
+            name: name.into(),
+            renaming: false,
+            enabled: true,
+            hue: 0,
+            intensity: 50,
+            temperature: 0,
+            mode: LightMode::Cct,
+        }
+    }
+}
+
+async fn run_old() -> Result<()> {
     let manager = Manager::new().await?;
 
     let adapters = manager.adapters().await?;
