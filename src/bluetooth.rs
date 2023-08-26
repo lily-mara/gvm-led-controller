bf146b01b7906cf8f6aefad28dc4e115c1d58e83
Lily Mara <lilymara@fastmail.com>
2023-08-26 16:30:04 -0700

    implement mac address discovery, try health check

--- src/bluetooth.rs
+++ src/bluetooth.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::HashSet,
+    fmt::Debug,
     sync::{Arc, Mutex},
     time::Duration,
 };
@@ -15,7 +16,7 @@ use btleplug::{
 };
 use eyre::{bail, eyre, Result};
 use futures::{pin_mut, stream::StreamExt, Stream};
-use tokio::{sync::mpsc::channel, time::sleep};
+use tokio::{select, sync::mpsc::channel, time::sleep};
 use tokio_stream::wrappers::ReceiverStream;
 use tracing::{error, info, trace, warn};
 
@@ -71,8 +72,13 @@ pub(crate) async fn scan_and_spawn(lights: Arc<Mutex<Vec<LightGuiState>>>) {
             }
         };
 
+        let name = match led.mac {
+            MacAddress::Unknown => String::from("New LED"),
+            MacAddress::Known(_) => format!("{:?}", led.mac),
+        };
+
         let (tx, rx) = channel(10);
-        let gui_state = LightGuiState::new("New LED", tx);
+        let gui_state = LightGuiState::new(name, tx);
 
         {
             lights.lock().unwrap().push(gui_state);
@@ -80,7 +86,7 @@ pub(crate) async fn scan_and_spawn(lights: Arc<Mutex<Vec<LightGuiState>>>) {
 
         let rx = debounced::debounced(ReceiverStream::new(rx), Duration::from_millis(100));
 
-        tokio::spawn(connection(led, rx));
+        tokio::spawn(led.connection(rx));
     }
 
     warn!("Scanning stream hung up");
@@ -91,6 +97,32 @@ pub(crate) async fn scan_and_spawn(lights: Arc<Mutex<Vec<LightGuiState>>>) {
 struct Led {
     peripheral: Peripheral,
     characteristic: Characteristic,
+
+    // CoreBluetooth hides the mac address of bluetooth accessories, so the only
+    // way to get the mac address is to inspect the device properties. This is
+    // done once at connection initialization time and the full mac address is
+    // stored here.
+    mac: MacAddress,
+}
+
+#[derive(PartialEq)]
+enum MacAddress {
+    Known([u8; 6]),
+    Unknown,
+}
+
+impl Debug for MacAddress {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Known(data) => {
+                for byte in &data[..5] {
+                    write!(f, "{byte:02x}:")?;
+                }
+                write!(f, "{:02x}", data[5])
+            }
+            Self::Unknown => write!(f, "Unknown"),
+        }
+    }
 }
 
 impl Led {
@@ -98,9 +130,9 @@ impl Led {
     async fn cmd(&self, command: impl Packable) -> Result<()> {
         let data = command.to_wire();
         trace!(
-            peripheral_id = ?self.peripheral.id(),
-            peripheral_mac = %self.peripheral.address(),
-            message = %format!("{data:02x?}"),
+            peripheral_id = %self.peripheral.id(),
+            peripheral_mac = ?self.mac,
+            raw = %format!("{data:02x?}"),
             ?command,
             "write"
         );
@@ -110,23 +142,122 @@ impl Led {
 
         Ok(())
     }
-}
 
-/// Listens forever to a stream which yields state changes for a given light and
-/// applies those state changes.
-async fn connection(led: Led, state_stream: impl Stream<Item = LightSettingsState>) -> Result<()> {
-    pin_mut!(state_stream);
+    async fn discover_mac(&mut self) -> Result<()> {
+        let properties = self
+            .peripheral
+            .properties()
+            .await?
+            .ok_or_else(|| eyre!("device had no properties"))?;
 
-    let mut previous_state = LightSettingsState::default();
-    write_state_no_cmp(&led, &previous_state).await?;
+        for (prefix, suffix) in properties.manufacturer_data {
+            if suffix.len() != 4 {
+                continue;
+            }
 
-    while let Some(state) = state_stream.next().await {
-        write_state(&led, &state, &previous_state).await?;
+            let [prefix_low, prefix_high] = prefix.to_le_bytes();
 
-        previous_state = state;
+            let mac = [
+                prefix_low,
+                prefix_high,
+                suffix[0],
+                suffix[1],
+                suffix[2],
+                suffix[3],
+            ];
+
+            self.mac = MacAddress::Known(mac);
+            break;
+        }
+
+        Ok(())
     }
 
-    Ok(())
+    /// Ensure the connection is healthy and attempt to reconnect if not
+    async fn health_check(&self) {
+        if let Ok(false) = self.peripheral.is_connected().await {
+            warn!(
+                peripheral_id = %self.peripheral.id(),
+                peripheral_mac = ?self.mac,
+                "LED disconnected"
+            );
+
+            loop {
+                if let Err(e) = self.peripheral.connect().await {
+                    warn!(
+                        peripheral_id = %self.peripheral.id(),
+                        peripheral_mac = ?self.mac,
+                        error=?e,
+                        "Failed to reconnect"
+                    );
+                    tokio::time::sleep(Duration::from_secs(5)).await;
+
+                    continue;
+                }
+
+                if let Ok(true) = self.peripheral.is_connected().await {
+                    info!(
+                        peripheral_id = %self.peripheral.id(),
+                        peripheral_mac = ?self.mac,
+                        "Reconnected"
+                    );
+                    break;
+                }
+            }
+        }
+    }
+
+    /// Listens forever to a stream which yields state changes for a given light and
+    /// applies those state changes.
+    async fn connection(
+        mut self,
+        state_stream: impl Stream<Item = LightSettingsState>,
+    ) -> Result<()> {
+        let mut previous_state = LightSettingsState::default();
+        write_state_no_cmp(&self, &previous_state).await?;
+
+        let mut health_interval = tokio::time::interval(Duration::from_secs(1));
+
+        pin_mut!(state_stream);
+
+        self.peripheral.subscribe(&self.characteristic).await?;
+
+        let mut notifications = self.peripheral.notifications().await?;
+
+        loop {
+            select! {
+                next = state_stream.next() => {
+                    let state = match next {
+                        None => break,
+                        Some(x) => x,
+                    };
+
+                    write_state(&self, &state, &previous_state).await?;
+
+                    previous_state = state;
+                }
+                _ = health_interval.tick() => {
+                    if self.mac == MacAddress::Unknown {
+                        _ = self.discover_mac().await;
+                    }
+                    self.health_check().await;
+                }
+                next = notifications.next() => {
+                    if let Some(notif) = next {
+                        trace!(
+                            peripheral_id = %self.peripheral.id(),
+                            peripheral_mac = ?self.mac,
+                            ?notif.value,
+                            %notif.uuid,
+                            "notification"
+                        );
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
 
 async fn write_state_no_cmp(led: &Led, state: &LightSettingsState) -> Result<()> {
@@ -261,15 +392,20 @@ fn scan_forever() -> impl Stream<Item = Result<Led>> {
                 }
 
                 peripheral.connect().await?;
-                info!(
-                    peripheral_id = ?peripheral.id(),
-                    peripheral_mac = %peripheral.address(),
-                    "connected"
-                );
 
                 let characteristic = find_characteristic(&peripheral).await?;
 
-                yield Ok(Led{ peripheral, characteristic })
+                let mut led = Led{ peripheral, characteristic, mac: MacAddress::Unknown };
+
+                _ = led.discover_mac().await;
+
+                info!(
+                    peripheral_id = %led.peripheral.id(),
+                    peripheral_mac = ?led.mac,
+                    "connected"
+                );
+
+                yield Ok(led)
             }
 
             tokio::time::sleep(std::time::Duration::from_millis(500)).await;
